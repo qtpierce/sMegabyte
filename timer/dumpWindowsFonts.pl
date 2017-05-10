cfff409f0e1258a1feb57dcff3aff769c6a9fdbe
qtpierce <qtpierce@gmail.com>
2017-05-09 23:03:06 -0700

    Refactored.
    Brought dumpWindowsFont.pl up to some latest coding standards.  Added a
    while loop, comments, and usage.

--- timer/dumpWindowsFonts.pl
+++ timer/dumpWindowsFonts.pl
@@ -1,12 +1,32 @@
 #! c:/utils/strawberry/perl/bin/
-#
+
+# Lists all the fonts found on the computer and listed in the Registry.
+
+# Usage:  
+#   1.  Copy this script to a computer's c:/temp/ directory.
+#   2.  Open up cmd.exe and cd to the location.
+#   3.  Run the script locally.
+
+
 use strict;
 use Win32::TieRegistry;
-$Registry->Delimiter("\\"); # Set delimiter to "/".
+
 $|=1;
-my $fontskey = $Registry->{"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts"} || die $^E;
+
+$Registry->Delimiter("\\");  # Set delimiter to "/".
+
+my $fontskey = $Registry->{
+        "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Fonts"
+    } || die $^E;
+
 for my $val ($fontskey->ValueNames)
 {
     print "$val\n";
 }
-exit;
+
+print "\n\nUse Ctrl+C to exit the final loop.\n";
+
+while (1)
+{
+    ;
+}
